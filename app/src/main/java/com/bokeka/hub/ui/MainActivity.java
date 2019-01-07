8ead949b1d89abaf7cbb541ae208b8970ca3772c
ishidai <prosperdai5@gmail.com>
2019-01-07 11:43:53 +0800

    底部导航栏和侧边栏搭建🐶

--- app/src/main/java/com/bokeka/hub/ui/MainActivity.java
+++ app/src/main/java/com/bokeka/hub/ui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.bokeka.hub.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.bokeka.hub.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * @author daixin 2019/1/5
@@ -16,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_activity_main);
+        setSupportActionBar(toolbar);
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
     }
 }
